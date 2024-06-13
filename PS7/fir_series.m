79213d330891f5127499cd87d18fccda591194dd
Wojciech <168547830+Yanosikk@users.noreply.github.com>
2024-06-14 01:16:26 +0200

    Delete PS7 directory
    Bad file

--- PS7/fir_series.m
+++
@@ -1,3 +0,0 @@
-function b = fir_series(b1, b2)
-b =conv(b1, b2);
-endfunction
--- PS7/gen_delta.m
+++
@@ -1,5 +0,0 @@
-function signal = gen_delta(N)
-signal = zeros(1, N);
-mid = (N - 1) / 2 + 1;
-signal(mid) = 1;
-endfunction
